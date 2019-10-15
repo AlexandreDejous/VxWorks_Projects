fc56e3b20af6724e194c50fc83df49cb94839977
dejouale <dejouale@k09-207.felk.cvut.cz>
2019-10-15 21:22:20 +0200

    project3

---
+++ TestProject3/SIMPENTIUMgnu_RTP/TestProject3/Debug/Objects/TestProject3/test3.d
@@ -0,0 +1,233 @@
+TestProject3/Debug/Objects/TestProject3/test3.o:  \
+ /home/dejouale/WindRiver/workspace/TestProject3/test3.c \
+  /opt/WindRiver/vxworks-6.9/target/usr/h/taskLib.h \
+  /opt/WindRiver/vxworks-6.9/target/usr/h/vxWorks.h \
+  /opt/WindRiver/vxworks-6.9/target/usr/h/vxWorksCommon.h \
+  /opt/WindRiver/vxworks-6.9/target/usr/h/types/vxCpu.h \
+  /opt/WindRiver/vxworks-6.9/target/usr/h/types/vxCpuX.h \
+  /opt/WindRiver/vxworks-6.9/target/usr/h/types/vxCpuXp.h \
+  /opt/WindRiver/vxworks-6.9/target/usr/h/types/vxCpuXf.h \
+  /opt/WindRiver/vxworks-6.9/target/usr/h/types/vxArch.h \
+  /opt/WindRiver/vxworks-6.9/target/usr/h/arch/simpentium/archSimpentium.h \
+  /opt/WindRiver/vxworks-6.9/target/usr/h/arch/simpentium/archSimpentiumCommon.h \
+  /opt/WindRiver/vxworks-6.9/target/usr/h/types/vxParams.h \
+  /opt/WindRiver/vxworks-6.9/target/usr/h/types/vxTypesBase.h \
+  /opt/WindRiver/gnu/4.3.3-vxworks-6.9/x86-linux2/bin/../../lib/gcc/i586-wrs-vxworks/4.3.3/include/stddef.h \
+  /opt/WindRiver/vxworks-6.9/target/usr/h/types/vxTypes.h \
+  /opt/WindRiver/vxworks-6.9/target/usr/h/stdint.h \
+  /opt/WindRiver/vxworks-6.9/target/usr/h/yvals.h \
+  /opt/WindRiver/vxworks-6.9/target/usr/h/base/b_ssize_t.h \
+  /opt/WindRiver/vxworks-6.9/target/usr/h/vsbConfig.h \
+  /opt/WindRiver/vxworks-6.9/target/lib/h/config/vsbConfig.h \
+  /opt/WindRiver/vxworks-6.9/target/lib/h/config/autoconf.h \
+  /opt/WindRiver/vxworks-6.9/target/usr/h/base/b_cpuset_t.h \
+  /opt/WindRiver/vxworks-6.9/target/usr/h/base/b_atomic_t.h \
+  /opt/WindRiver/vxworks-6.9/target/usr/h/base/b_dev_t.h \
+  /opt/WindRiver/vxworks-6.9/target/usr/h/base/b_gid_t.h \
+  /opt/WindRiver/vxworks-6.9/target/usr/h/base/b_ino_t.h \
+  /opt/WindRiver/vxworks-6.9/target/usr/h/base/b_mode_t.h \
+  /opt/WindRiver/vxworks-6.9/target/usr/h/base/b_nlink_t.h \
+  /opt/WindRiver/vxworks-6.9/target/usr/h/base/b_off_t.h \
+  /opt/WindRiver/vxworks-6.9/target/usr/h/base/b_pid_t.h \
+  /opt/WindRiver/vxworks-6.9/target/usr/h/base/b_uid_t.h \
+  /opt/WindRiver/vxworks-6.9/target/usr/h/base/b_blksize_t.h \
+  /opt/WindRiver/vxworks-6.9/target/usr/h/base/b_blkcnt_t.h \
+  /opt/WindRiver/vxworks-6.9/target/usr/h/types/vxTypesOld.h \
+  /opt/WindRiver/vxworks-6.9/target/usr/h/base/e_vxTypesOld.h \
+  /opt/WindRiver/vxworks-6.9/target/usr/h/sys/types.h \
+  /opt/WindRiver/vxworks-6.9/target/usr/h/base/b_pthread_attr_t.h \
+  /opt/WindRiver/vxworks-6.9/target/usr/h/base/b_size_t.h \
+  /opt/WindRiver/vxworks-6.9/target/usr/h/base/b_struct__Sched_param.h \
+  /opt/WindRiver/vxworks-6.9/target/usr/h/base/b_struct__Timespec.h \
+  /opt/WindRiver/vxworks-6.9/target/usr/h/base/b_time_t.h \
+  /opt/WindRiver/vxworks-6.9/target/usr/h/base/b_clock_t.h \
+  /opt/WindRiver/vxworks-6.9/target/usr/h/base/b_key_t.h \
+  /opt/WindRiver/vxworks-6.9/target/usr/h/base/b_timer_t.h \
+  /opt/WindRiver/vxworks-6.9/target/usr/h/base/b_ptrdiff_t.h \
+  /opt/WindRiver/vxworks-6.9/target/usr/h/base/b_clockid_t.h \
+  /opt/WindRiver/vxworks-6.9/target/usr/h/base/b_pthread_t.h \
+  /opt/WindRiver/vxworks-6.9/target/usr/h/base/b_pthread_cond_t.h \
+  /opt/WindRiver/vxworks-6.9/target/usr/h/types/vxWind.h \
+  /opt/WindRiver/vxworks-6.9/target/usr/h/types/vxWindCommon.h \
+  /opt/WindRiver/vxworks-6.9/target/usr/h/base/b_pthread_condattr_t.h \
+  /opt/WindRiver/vxworks-6.9/target/usr/h/base/b_pthread_mutex_t.h \
+  /opt/WindRiver/vxworks-6.9/target/usr/h/base/b_pthread_mutexattr_t.h \
+  /opt/WindRiver/vxworks-6.9/target/usr/h/base/b_pthread_key_t.h \
+  /opt/WindRiver/vxworks-6.9/target/usr/h/base/b_pthread_once_t.h \
+  /opt/WindRiver/vxworks-6.9/target/usr/h/base/b_BOOL.h \
+  /opt/WindRiver/vxworks-6.9/target/usr/h/base/b_suseconds_t.h \
+  /opt/WindRiver/vxworks-6.9/target/usr/h/base/b_trace_attr_t.h \
+  /opt/WindRiver/vxworks-6.9/target/usr/h/base/b_trace_id_t.h \
+  /opt/WindRiver/vxworks-6.9/target/usr/h/base/b_trace_event_id_t.h \
+  /opt/WindRiver/vxworks-6.9/target/usr/h/base/b_trace_event_set_t.h \
+  /opt/WindRiver/vxworks-6.9/target/usr/h/tool/gnu/toolMacros.h \
+  /opt/WindRiver/vxworks-6.9/target/usr/h/tool/common/private/toolMacrosP.h \
+  /opt/WindRiver/vxworks-6.9/target/usr/h/taskLibCommon.h \
+  /opt/WindRiver/vxworks-6.9/target/usr/h/vwModNum.h \
+  /opt/WindRiver/vxworks-6.9/target/usr/h/eventLibCommon.h \
+  /opt/WindRiver/vxworks-6.9/target/usr/h/vxWorks.h \
+  /opt/WindRiver/vxworks-6.9/target/usr/h/vwModNum.h \
+  /opt/WindRiver/vxworks-6.9/target/usr/h/objLib.h \
+  /opt/WindRiver/vxworks-6.9/target/usr/h/objLibCommon.h \
+  /opt/WindRiver/vxworks-6.9/target/usr/h/objSysCall.h \
+  /opt/WindRiver/vxworks-6.9/target/usr/h/arch/simpentium/coprocSimpentium.h \
+  /opt/WindRiver/vxworks-6.9/target/usr/h/taskSysCall.h \
+  /opt/WindRiver/vxworks-6.9/target/usr/h/stdio.h \
+  /opt/WindRiver/vxworks-6.9/target/usr/h/semLib.h \
+  /opt/WindRiver/vxworks-6.9/target/usr/h/semLibCommon.h \
+  /opt/WindRiver/vxworks-6.9/target/usr/h/semSysCall.h \
+  /opt/WindRiver/vxworks-6.9/target/usr/h/stdlib.h
+
+/opt/WindRiver/vxworks-6.9/target/usr/h/taskLib.h:
+
+/opt/WindRiver/vxworks-6.9/target/usr/h/vxWorks.h:
+
+/opt/WindRiver/vxworks-6.9/target/usr/h/vxWorksCommon.h:
+
+/opt/WindRiver/vxworks-6.9/target/usr/h/types/vxCpu.h:
+
+/opt/WindRiver/vxworks-6.9/target/usr/h/types/vxCpuX.h:
+
+/opt/WindRiver/vxworks-6.9/target/usr/h/types/vxCpuXp.h:
+
+/opt/WindRiver/vxworks-6.9/target/usr/h/types/vxCpuXf.h:
+
+/opt/WindRiver/vxworks-6.9/target/usr/h/types/vxArch.h:
+
+/opt/WindRiver/vxworks-6.9/target/usr/h/arch/simpentium/archSimpentium.h:
+
+/opt/WindRiver/vxworks-6.9/target/usr/h/arch/simpentium/archSimpentiumCommon.h:
+
+/opt/WindRiver/vxworks-6.9/target/usr/h/types/vxParams.h:
+
+/opt/WindRiver/vxworks-6.9/target/usr/h/types/vxTypesBase.h:
+
+/opt/WindRiver/gnu/4.3.3-vxworks-6.9/x86-linux2/bin/../../lib/gcc/i586-wrs-vxworks/4.3.3/include/stddef.h:
+
+/opt/WindRiver/vxworks-6.9/target/usr/h/types/vxTypes.h:
+
+/opt/WindRiver/vxworks-6.9/target/usr/h/stdint.h:
+
+/opt/WindRiver/vxworks-6.9/target/usr/h/yvals.h:
+
+/opt/WindRiver/vxworks-6.9/target/usr/h/base/b_ssize_t.h:
+
+/opt/WindRiver/vxworks-6.9/target/usr/h/vsbConfig.h:
+
+/opt/WindRiver/vxworks-6.9/target/lib/h/config/vsbConfig.h:
+
+/opt/WindRiver/vxworks-6.9/target/lib/h/config/autoconf.h:
+
+/opt/WindRiver/vxworks-6.9/target/usr/h/base/b_cpuset_t.h:
+
+/opt/WindRiver/vxworks-6.9/target/usr/h/base/b_atomic_t.h:
+
+/opt/WindRiver/vxworks-6.9/target/usr/h/base/b_dev_t.h:
+
+/opt/WindRiver/vxworks-6.9/target/usr/h/base/b_gid_t.h:
+
+/opt/WindRiver/vxworks-6.9/target/usr/h/base/b_ino_t.h:
+
+/opt/WindRiver/vxworks-6.9/target/usr/h/base/b_mode_t.h:
+
+/opt/WindRiver/vxworks-6.9/target/usr/h/base/b_nlink_t.h:
+
+/opt/WindRiver/vxworks-6.9/target/usr/h/base/b_off_t.h:
+
+/opt/WindRiver/vxworks-6.9/target/usr/h/base/b_pid_t.h:
+
+/opt/WindRiver/vxworks-6.9/target/usr/h/base/b_uid_t.h:
+
+/opt/WindRiver/vxworks-6.9/target/usr/h/base/b_blksize_t.h:
+
+/opt/WindRiver/vxworks-6.9/target/usr/h/base/b_blkcnt_t.h:
+
+/opt/WindRiver/vxworks-6.9/target/usr/h/types/vxTypesOld.h:
+
+/opt/WindRiver/vxworks-6.9/target/usr/h/base/e_vxTypesOld.h:
+
+/opt/WindRiver/vxworks-6.9/target/usr/h/sys/types.h:
+
+/opt/WindRiver/vxworks-6.9/target/usr/h/base/b_pthread_attr_t.h:
+
+/opt/WindRiver/vxworks-6.9/target/usr/h/base/b_size_t.h:
+
+/opt/WindRiver/vxworks-6.9/target/usr/h/base/b_struct__Sched_param.h:
+
+/opt/WindRiver/vxworks-6.9/target/usr/h/base/b_struct__Timespec.h:
+
+/opt/WindRiver/vxworks-6.9/target/usr/h/base/b_time_t.h:
+
+/opt/WindRiver/vxworks-6.9/target/usr/h/base/b_clock_t.h:
+
+/opt/WindRiver/vxworks-6.9/target/usr/h/base/b_key_t.h:
+
+/opt/WindRiver/vxworks-6.9/target/usr/h/base/b_timer_t.h:
+
+/opt/WindRiver/vxworks-6.9/target/usr/h/base/b_ptrdiff_t.h:
+
+/opt/WindRiver/vxworks-6.9/target/usr/h/base/b_clockid_t.h:
+
+/opt/WindRiver/vxworks-6.9/target/usr/h/base/b_pthread_t.h:
+
+/opt/WindRiver/vxworks-6.9/target/usr/h/base/b_pthread_cond_t.h:
+
+/opt/WindRiver/vxworks-6.9/target/usr/h/types/vxWind.h:
+
+/opt/WindRiver/vxworks-6.9/target/usr/h/types/vxWindCommon.h:
+
+/opt/WindRiver/vxworks-6.9/target/usr/h/base/b_pthread_condattr_t.h:
+
+/opt/WindRiver/vxworks-6.9/target/usr/h/base/b_pthread_mutex_t.h:
+
+/opt/WindRiver/vxworks-6.9/target/usr/h/base/b_pthread_mutexattr_t.h:
+
+/opt/WindRiver/vxworks-6.9/target/usr/h/base/b_pthread_key_t.h:
+
+/opt/WindRiver/vxworks-6.9/target/usr/h/base/b_pthread_once_t.h:
+
+/opt/WindRiver/vxworks-6.9/target/usr/h/base/b_BOOL.h:
+
+/opt/WindRiver/vxworks-6.9/target/usr/h/base/b_suseconds_t.h:
+
+/opt/WindRiver/vxworks-6.9/target/usr/h/base/b_trace_attr_t.h:
+
+/opt/WindRiver/vxworks-6.9/target/usr/h/base/b_trace_id_t.h:
+
+/opt/WindRiver/vxworks-6.9/target/usr/h/base/b_trace_event_id_t.h:
+
+/opt/WindRiver/vxworks-6.9/target/usr/h/base/b_trace_event_set_t.h:
+
+/opt/WindRiver/vxworks-6.9/target/usr/h/tool/gnu/toolMacros.h:
+
+/opt/WindRiver/vxworks-6.9/target/usr/h/tool/common/private/toolMacrosP.h:
+
+/opt/WindRiver/vxworks-6.9/target/usr/h/taskLibCommon.h:
+
+/opt/WindRiver/vxworks-6.9/target/usr/h/vwModNum.h:
+
+/opt/WindRiver/vxworks-6.9/target/usr/h/eventLibCommon.h:
+
+/opt/WindRiver/vxworks-6.9/target/usr/h/vxWorks.h:
+
+/opt/WindRiver/vxworks-6.9/target/usr/h/vwModNum.h:
+
+/opt/WindRiver/vxworks-6.9/target/usr/h/objLib.h:
+
+/opt/WindRiver/vxworks-6.9/target/usr/h/objLibCommon.h:
+
+/opt/WindRiver/vxworks-6.9/target/usr/h/objSysCall.h:
+
+/opt/WindRiver/vxworks-6.9/target/usr/h/arch/simpentium/coprocSimpentium.h:
+
+/opt/WindRiver/vxworks-6.9/target/usr/h/taskSysCall.h:
+
+/opt/WindRiver/vxworks-6.9/target/usr/h/stdio.h:
+
+/opt/WindRiver/vxworks-6.9/target/usr/h/semLib.h:
+
+/opt/WindRiver/vxworks-6.9/target/usr/h/semLibCommon.h:
+
+/opt/WindRiver/vxworks-6.9/target/usr/h/semSysCall.h:
+
+/opt/WindRiver/vxworks-6.9/target/usr/h/stdlib.h:
